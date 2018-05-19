7c425bb33926ab32b39cd358ab17ced5f9c100c2
Arthur <web@quokka.pro>
2018-05-19 16:30:40 +0300

    popup

--- app/js/common.js
+++ app/js/common.js
@@ -35,4 +35,29 @@ morphTimeline
   .to($('#megaphone'),2,{morphSVG:{shape:"#megaphone"}, fill:"none"},"+=2")
 ;
 */
+/* =================== popup ======================= */
+$(function() {
+
+	$(".popup").magnificPopup();
+
+	//E-mail Ajax Send
+	$("#form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Спасибо мы с вами свяжемся в течении 2 минут!");
+			setTimeout(function() {
+				$.magnificPopup.close();
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+		debugger;
+
+		});
+
 
