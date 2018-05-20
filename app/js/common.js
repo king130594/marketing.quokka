6cad050a5d23799894587d4af8f4e83ebc91c675
Arthur <web@quokka.pro>
2018-05-20 21:47:45 +0300

    mobile alpha 0.8.1

--- app/js/common.js
+++ app/js/common.js
@@ -21,32 +21,11 @@ $(document).ready(function() {
 			});
 		});
 
-/*=================================== menu head ===========================*/
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 1){  
-        $('#header').addClass("sticky");
-    }
-    else{
-        $('#header').removeClass("sticky");
-    }
-});
-
-/*========================== image for section ==============================
 
 
-var morphTimeline = new TimelineMax({
-  repeat:-1,
-  repeatDelay:2
-});
 
-morphTimeline
-  .timeScale(1.5)
-  .to($('#megaphone'),2,{morphSVG:{shape:"#links"}, fill:"none",opacity:1})
-  .to($('#megaphone'),2,{morphSVG:{shape:"#computer"}, fill:"none"},"+=2")
-  .to($('#megaphone'),2,{morphSVG:{shape:"#light"}, fill:"none"},"+=2")
-  .to($('#megaphone'),2,{morphSVG:{shape:"#megaphone"}, fill:"none"},"+=2")
-;
-*/
+
+
 /* =================== popup ======================= */
 $(function() {
 
@@ -72,4 +51,11 @@ $(function() {
 
 		});
 
+/*============================toggle mobile ==========================*/
+
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $("ul").toggle();
+    });
+});
 
